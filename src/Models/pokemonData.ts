95173d8c22eade5a4c6f586d345e3edb7ae8c117
Peter Hu <firemedalj1@hotmail.com>
2024-12-26 18:11:38 -0500

    init

---
+++ src/Models/pokemonData.ts
@@ -0,0 +1,23 @@
+export type PokemonData = {
+  name: string;
+  sprites: {
+    back_default: string;
+    back_female: string | null;
+    back_shiny: string | null;
+    back_shiny_female: string | null;
+    front_default: string;
+    front_female: string | null;
+    front_shiny: string | null;
+    front_shiny_female: string | null;
+  };
+  abilities: [PokemonAbilityData];
+};
+
+type PokemonAbilityData = {
+  ability: {
+    name: string;
+    url: string;
+  };
+  is_hidden: boolean;
+  slot: number;
+}
---
+++ src/Pages/Pokemon.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react"
+import {
+  Box,
+  Text,
+  VStack,
+  Grid,
+  Image,
+  Table,
+  Flex
+} from "@chakra-ui/react"
+import { client } from 'axiosClient'
+import { PokemonData } from "Models/pokemonData"
+import { useParams } from "react-router-dom"
+
+export const PokemonPage = () => {
+
+  const params = useParams();
+  const pokemonName = params.pokemonName;
+  const [pokemonData, setPokemonData] = useState<PokemonData>()
+
+  useEffect(() => {
+    client.get(`pokemon/${pokemonName}`).then((res) => { setPokemonData(res.data)})
+  }, []);
+
+  return (
+    <Box textAlign={"center"} fontSize="xl">
+      <Grid alignContent={"center"} minH="100vh" p={3}>
+        <VStack>
+          {
+          pokemonData?
+          <>
+          <Image height={"200px"} width={"200px"} src={pokemonData.sprites.front_default} />
+          <Text>
+            {pokemonData.name}
+          </Text>
+          <Flex alignSelf={"flex-end"} width={pokemonData.abilities.length * 200}>
+            <Table.Root size={"md"} striped showColumnBorder>
+              <Table.Header>
+                <Table.Row>
+                {pokemonData.abilities.map(ability=>{
+                  return <Table.ColumnHeader key={ability.slot}>
+                  {
+                  ability.slot == 1 ? <Text> Ability </Text>
+                  :
+                  ability.slot == 2 ? <Text> 2nd Ability </Text>
+                  :
+                  <Text> Hidden Ability </Text>
+                  }
+                  </Table.ColumnHeader>
+                })}
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                <Table.Row>
+                {pokemonData.abilities.map(ability=>{
+                  return <Table.Cell key={ability.slot}> {ability.ability.name} </Table.Cell>
+                })}
+                </Table.Row>
+              </Table.Body>
+            </Table.Root>
+          </Flex>
+          </>
+          :
+          <Text>Now Loading...</Text>
+          }
+        </VStack>
+      </Grid>
+    </Box>
+  )
+}
