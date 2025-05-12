d74f58b0cf2bde9f3a966a4a164b7e73ead060a5
Peter Hu <firemedalj1@hotmail.com>
2025-05-12 17:14:53 -0400

    changes

--- pages/pokemon/[name].tsx
+++ pages/pokemon/[name].tsx
@@ -1,10 +1,11 @@
-import { Container, Table } from "@nextui-org/react"
-import { client } from "common/axiosClient"
+import { Container, Table } from "@nextui/react"
 import { fetchPokemonData, fetchPokemonNames } from "common/pokemonNames"
 import type { PokemonData } from "models/pokemonData"
 import { TypeData } from "models/typeData"
+import Head from "next/head"
 import Image from "next/image"
 import React from "react"
+import styles from "styles/Home.module.css"
 
 interface Props {
   pokemonData: PokemonData;
@@ -25,74 +26,85 @@ const PokemonPage = ({ pokemonData, typeData }: Props) => {
         splitName[0] = "MEGA"
         newName = splitName.join(" ");
       }
-      else {newName = name;}
+      else { newName = name; }
     }
-    else {newName = name;}
+    else { newName = name; }
     return newName;
   }
 
+  let newName: any
+  newName = renderPokemonName()
+
   return (
-    <Container
-        display="flex"
-        direction="column"
-        alignContent="center"
-        alignItems="center"
-    >
-      <Container
-        display="flex"
-        direction="column"
-        alignContent="center"
-        alignItems="center"
-      >
-        <Image
-          alt={"pokemonImage"}
-          height={250}
-          width={250}
-          src={pokemonData.sprites.front_default}
-        />
-        <h1>{renderPokemonName()}</h1>
-        {typeData.map(type =>
-          <Image
-          style={{margin: "8px"}}
-          key={type.slot}
-          alt={type.data.name}
-          height={30}
-          width={150}
-          src={type.data.sprites["generation-ix"]["scarlet-violet"].name_icon}
-          />
-        )}
-      </Container>
-      <div style={{width: "50%"}}>
-        <Table striped aria-label="Abilities Table">
-          <Table.Header>
-            {pokemonData.abilities.map((ability) => {
-              return (
-                <Table.Column key={ability.slot}>
-                  {ability.slot == 1 ? (
-                    <> Ability </>
-                  ) : ability.slot == 2 ? (
-                    <> 2nd Ability </>
-                  ) : (
-                    <> Hidden Ability </>
-                  )}
-                </Table.Column>
-              )
-            })}
-          </Table.Header>
-          <Table.Body>
-            <Table.Row>
-              {pokemonData.abilities.map((ability) => {
-                return (
-                  <Table.Cell key={ability.slot}>
-                    {ability.ability.name}
-                  </Table.Cell>
-                )
-              })}
-            </Table.Row>
-          </Table.Body>
-        </Table>
-      </div>
-    </Container>
+    <div className={styles.container}>
+      <Head>
+        <title>{newName}</title>
+        <link rel="icon" href="/Pokeball.ico" />
+      </Head>
+      <main className={styles.main}>
+        <Container
+          display="flex"
+          direction="column"
+          alignContent="center"
+          alignItems="center"
+        >
+          <Container
+            display="flex"
+            direction="column"
+            alignContent="center"
+            alignItems="center"
+          >
+            <Image
+              alt={"pokemonImage"}
+              height={250}
+              width={250}
+              src={pokemonData.sprites.front_default}
+            />
+            <h1>{newName}</h1>
+            {typeData.map(type =>
+              <Image
+                style={{ margin: "8px" }}
+                key={type.slot}
+                alt={type.data.name}
+                height={30}
+                width={150}
+                src={type.data.sprites["generation-ix"]["scarlet-violet"].name_icon}
+              />
+            )}
+          </Container>
+          <div style={{ width: "50%" }}>
+            <Table striped aria-label="Abilities Table">
+              <Table.Header>
+                {pokemonData.abilities.map((ability) => {
+                  return (
+                    <Table.Column key={ability.slot}>
+                      {ability.slot == 1 ? (
+                        <> Ability </>
+                      ) : ability.slot == 2 ? (
+                        <> 2nd Ability </>
+                      ) : (
+                        <> Hidden Ability </>
+                      )}
+                    </Table.Column>
+                  )
+                })}
+              </Table.Header>
+              <Table.Body>
+                <Table.Row>
+                  {pokemonData.abilities.map((ability) => {
+                    return (
+                      <Table.Cell key={ability.slot}>
+                        {ability.ability.name}
+                      </Table.Cell>
+                    )
+                  })}
+                </Table.Row>
+              </Table.Body>
+            </Table>
+          </div>
+        </Container>
+      </main>
+    </div>
   )
 }
 
