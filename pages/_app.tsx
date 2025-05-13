d5cff3ab69625926ea7a617fa8296995467b3cac
Peter Hu <91496682+HuPeter1@users.noreply.github.com>
2025-05-13 00:17:12 +0000

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import "../styles/globals.css"
 
 import { NextUIProvider } from "@nextui-org/react"
 import type { AppProps } from "next/app"
+import { Analytics } from '@vercel/analytics/next';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
       <Component {...pageProps} />
+      <Analytics />
     </NextUIProvider>
   )
 }
